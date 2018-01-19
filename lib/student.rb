76134bddc304e84f8376c2050b58d7cd654eecad
James Ferguson <ncjamesferguson@gmail.com>
2018-01-19 00:49:26 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -16,7 +16,7 @@ class Student
     end
 
     def self.create
-      "CREATE TABLE IF NOT EXISTS students (id INTEGER PRIMARY KEY, name TEXT, grade INTEGER);"
+      DB[:cpnn].execute("CREATE TABLE IF NOT EXISTS students (id INTEGER PRIMARY KEY, name TEXT, grade INTEGER);")
     end
 
     def self.drop_table
